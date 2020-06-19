eb54b63242016152c8ddfc6e0a1a0ec404d16b02
Nicholas O'Shea <nosh@amazon.com>
2020-06-19 16:27:47 +0000

    ReadHandler improvements

--- aws-rds-dbproxy/src/main/java/software/amazon/rds/dbproxy/ReadHandler.java
+++ aws-rds-dbproxy/src/main/java/software/amazon/rds/dbproxy/ReadHandler.java
@@ -1,9 +1,17 @@
 package software.amazon.rds.dbproxy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.amazonaws.services.rds.AmazonRDS;
 import com.amazonaws.services.rds.AmazonRDSClientBuilder;
+import com.amazonaws.services.rds.model.DBProxy;
 import com.amazonaws.services.rds.model.DescribeDBProxiesRequest;
 import com.amazonaws.services.rds.model.DescribeDBProxiesResult;
+import com.amazonaws.services.rds.model.ListTagsForResourceRequest;
+import com.amazonaws.services.rds.model.ListTagsForResourceResult;
+import com.amazonaws.services.rds.model.Tag;
+import com.google.common.collect.ImmutableList;
 import software.amazon.cloudformation.exceptions.CfnNotFoundException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
@@ -37,10 +45,36 @@ public class ReadHandler extends BaseHandler<CallbackContext> {
         DescribeDBProxiesRequest request = new DescribeDBProxiesRequest().withDBProxyName(proxyName);
 
         final DescribeDBProxiesResult result = clientProxy.injectCredentialsAndInvoke(request, rdsClient::describeDBProxies);
+
         if (result != null && result.getDBProxies() != null && result.getDBProxies().size() == 1) {
-            return Utility.resultToModel(result.getDBProxies().get(0));
+            DBProxy proxy = result.getDBProxies().get(0);
+            ResourceModel resourceModel = Utility.resultToModel(proxy);
+            ListTagsForResourceRequest tagRequest = new ListTagsForResourceRequest().withResourceName(proxy.getDBProxyArn());
+
+            final ListTagsForResourceResult tagResult = clientProxy.injectCredentialsAndInvoke(tagRequest, rdsClient::listTagsForResource);
+            if (tagResult != null && tagResult.getTagList()!= null) {
+                resourceModel.setTags(convertTags(tagResult.getTagList()));
+            }
+            return resourceModel;
         } else {
             throw new CfnNotFoundException(ResourceModel.TYPE_NAME, proxyName);
         }
+    }
+
+    private List<TagFormat> convertTags(List<Tag> tags) {
+        List<TagFormat> convertedTags = new ArrayList<>();
+
+        if (tags == null || tags.size() == 0) {
+            return convertedTags;
+        }
+
+        for(Tag tag : tags){
+            convertedTags.add(TagFormat.builder()
+                                       .key(tag.getKey())
+                                       .value(tag.getValue())
+                                       .build());
+        }
+
+        return convertedTags;
     }
 }
--- aws-rds-dbproxy/src/test/java/software/amazon/rds/dbproxy/ReadHandlerTest.java
+++ aws-rds-dbproxy/src/test/java/software/amazon/rds/dbproxy/ReadHandlerTest.java
@@ -18,6 +18,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.amazonaws.services.rds.model.DBProxy;
 import com.amazonaws.services.rds.model.DescribeDBProxiesRequest;
 import com.amazonaws.services.rds.model.DescribeDBProxiesResult;
+import com.amazonaws.services.rds.model.ListTagsForResourceRequest;
+import com.amazonaws.services.rds.model.ListTagsForResourceResult;
+import com.amazonaws.services.rds.model.Tag;
 import com.google.common.collect.ImmutableList;
 import software.amazon.cloudformation.exceptions.CfnNotFoundException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
@@ -45,12 +48,18 @@ public class ReadHandlerTest {
     public void handleRequest_SimpleSuccess() {
         final ReadHandler handler = new ReadHandler();
 
-        DBProxy proxy1 = new DBProxy().withDBProxyName("proxy1");
+        DBProxy proxy1 = new DBProxy().withDBProxyName("proxy1")
+                                      .withDBProxyArn("arn")
+                                      .withRoleArn("rolearn")
+                                      .withVpcSubnetIds("vpcsubnet1", "vpcsubnet2")
+                                      .withVpcSecurityGroupIds("sg1", "sg2");
         final List<DBProxy> existingProxies = ImmutableList.of(proxy1);
 
         doReturn(new DescribeDBProxiesResult().withDBProxies(existingProxies))
                 .when(proxy)
                 .injectCredentialsAndInvoke(any(DescribeDBProxiesRequest.class), any());
+
+        doReturn(new ListTagsForResourceResult()).when(proxy).injectCredentialsAndInvoke(any(ListTagsForResourceRequest.class), any());
 
         final ResourceModel model = ResourceModel.builder().dBProxyName("proxy1").build();
 
@@ -67,6 +76,55 @@ public class ReadHandlerTest {
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModels()).isNull();
         assertThatModelsAreEqual(response.getResourceModel(), proxy1);
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+    }
+
+    @Test
+    public void handleRequest_WithTags() {
+        final ReadHandler handler = new ReadHandler();
+
+        DBProxy proxy1 = new DBProxy().withDBProxyName("proxy1")
+                                      .withDBProxyArn("arn")
+                                      .withRoleArn("rolearn")
+                                      .withVpcSubnetIds("vpcsubnet1", "vpcsubnet2")
+                                      .withVpcSecurityGroupIds("sg1", "sg2");
+        final List<DBProxy> existingProxies = ImmutableList.of(proxy1);
+
+        doReturn(new DescribeDBProxiesResult().withDBProxies(existingProxies))
+                .when(proxy)
+                .injectCredentialsAndInvoke(any(DescribeDBProxiesRequest.class), any());
+
+        String tagKey = "tagKey";
+        String tagValue = "tagValue";
+        String tagKey2 = "tagKey2";
+        String tagValue2 = "tagValue2";
+        List<Tag> tagList = ImmutableList.of(new Tag().withKey(tagKey).withValue(tagValue),
+                                             new Tag().withKey(tagKey2).withValue(tagValue2));
+        doReturn(new ListTagsForResourceResult().withTagList(tagList))
+                .when(proxy)
+                .injectCredentialsAndInvoke(any(ListTagsForResourceRequest.class), any());
+
+        final ResourceModel model = ResourceModel.builder().dBProxyName("proxy1").build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                                                                      .desiredResourceState(model)
+                                                                      .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThatModelsAreEqual(response.getResourceModel(), proxy1);
+        assertThat(response.getResourceModel().getTags().size()).isEqualTo(2);
+        assertThat(response.getResourceModel().getTags().get(0).getKey()).isEqualTo(tagKey);
+        assertThat(response.getResourceModel().getTags().get(0).getValue()).isEqualTo(tagValue);
+        assertThat(response.getResourceModel().getTags().get(1).getKey()).isEqualTo(tagKey2);
+        assertThat(response.getResourceModel().getTags().get(1).getValue()).isEqualTo(tagValue2);
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
     }

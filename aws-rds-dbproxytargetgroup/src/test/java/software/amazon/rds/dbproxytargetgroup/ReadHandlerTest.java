b2c5c547b796b9172b99c5a560634013e14d70cf
Nicholas O'Shea <nosh@amazon.com>
2020-06-23 17:05:24 +0000

    Add registered targets to ReadHandler

--- aws-rds-dbproxytargetgroup/src/test/java/software/amazon/rds/dbproxytargetgroup/ReadHandlerTest.java
+++ aws-rds-dbproxytargetgroup/src/test/java/software/amazon/rds/dbproxytargetgroup/ReadHandlerTest.java
@@ -1,10 +1,14 @@
 package software.amazon.rds.dbproxytargetgroup;
 
 import com.amazonaws.services.rds.model.ConnectionPoolConfigurationInfo;
+import com.amazonaws.services.rds.model.DBProxyTarget;
 import com.amazonaws.services.rds.model.DBProxyTargetGroup;
 import com.amazonaws.services.rds.model.DescribeDBProxiesRequest;
 import com.amazonaws.services.rds.model.DescribeDBProxyTargetGroupsRequest;
 import com.amazonaws.services.rds.model.DescribeDBProxyTargetGroupsResult;
+import com.amazonaws.services.rds.model.DescribeDBProxyTargetsRequest;
+import com.amazonaws.services.rds.model.DescribeDBProxyTargetsResult;
+import com.amazonaws.services.rds.model.TargetType;
 import com.google.common.collect.ImmutableList;
 import software.amazon.cloudformation.exceptions.CfnNotFoundException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
@@ -50,24 +54,27 @@ public class ReadHandlerTest {
     @Test
     public void handleRequest_SimpleSuccess() {
         final ReadHandler handler = new ReadHandler();
-        String customName = "customName";
         ConnectionPoolConfigurationInfo connectionPoolConfigurationInfo = new ConnectionPoolConfigurationInfo();
 
         DBProxyTargetGroup dbProxyTargetGroup1 = new DBProxyTargetGroup().withConnectionPoolConfig(connectionPoolConfigurationInfo)
                                                                          .withDBProxyName(PROXY_NAME)
-                                                                         .withTargetGroupName(customName);
+                                                                         .withTargetGroupName(DEFAULT_NAME);
         final List<DBProxyTargetGroup> existingProxies = ImmutableList.of(dbProxyTargetGroup1);
 
 
         DescribeDBProxyTargetGroupsRequest describeRequest = new DescribeDBProxyTargetGroupsRequest()
-                                                             .withDBProxyName(PROXY_NAME)
-                                                             .withTargetGroupName(customName);
+                                                                     .withDBProxyName(PROXY_NAME)
+                                                                     .withTargetGroupName(DEFAULT_NAME);
         doReturn(new DescribeDBProxyTargetGroupsResult().withTargetGroups(existingProxies))
                 .when(proxy)
                 .injectCredentialsAndInvoke(eq(describeRequest), any());
 
+        DescribeDBProxyTargetsRequest targetRequest = new DescribeDBProxyTargetsRequest()
+                                                              .withDBProxyName(PROXY_NAME)
+                                                              .withTargetGroupName(DEFAULT_NAME);
+        doReturn(new DescribeDBProxyTargetsResult()).when(proxy).injectCredentialsAndInvoke(eq(targetRequest), any());
 
-        final ResourceModel model = ResourceModel.builder().dBProxyName(PROXY_NAME).targetGroupName(customName).build();
+        final ResourceModel model = ResourceModel.builder().dBProxyName(PROXY_NAME).targetGroupName(DEFAULT_NAME).build();
 
         final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
                                                                       .desiredResourceState(model)
@@ -82,6 +89,57 @@ public class ReadHandlerTest {
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModels()).isNull();
         assertThatModelsAreEqual(response.getResourceModel(), dbProxyTargetGroup1);
+        assertThat(response.getResourceModel().getDBClusterIdentifiers()).isNull();
+        assertThat(response.getResourceModel().getDBInstanceIdentifiers()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+    }
+
+    @Test
+    public void handleRequest_WithRegisteredCluster() {
+        final ReadHandler handler = new ReadHandler();
+        ConnectionPoolConfigurationInfo connectionPoolConfigurationInfo = new ConnectionPoolConfigurationInfo();
+
+        DBProxyTargetGroup dbProxyTargetGroup1 = new DBProxyTargetGroup().withConnectionPoolConfig(connectionPoolConfigurationInfo)
+                                                                         .withDBProxyName(PROXY_NAME)
+                                                                         .withTargetGroupName(DEFAULT_NAME);
+        final List<DBProxyTargetGroup> existingProxies = ImmutableList.of(dbProxyTargetGroup1);
+
+
+        DescribeDBProxyTargetGroupsRequest describeRequest = new DescribeDBProxyTargetGroupsRequest()
+                                                                     .withDBProxyName(PROXY_NAME)
+                                                                     .withTargetGroupName(DEFAULT_NAME);
+        doReturn(new DescribeDBProxyTargetGroupsResult().withTargetGroups(existingProxies))
+                .when(proxy)
+                .injectCredentialsAndInvoke(eq(describeRequest), any());
+
+        DescribeDBProxyTargetsRequest targetRequest = new DescribeDBProxyTargetsRequest()
+                                                                   .withDBProxyName(PROXY_NAME)
+                                                                   .withTargetGroupName(DEFAULT_NAME);
+        String clusterId = "clusterId";
+        doReturn(new DescribeDBProxyTargetsResult()
+                         .withTargets(new DBProxyTarget().withType(TargetType.TRACKED_CLUSTER).withRdsResourceId(clusterId),
+                                      new DBProxyTarget().withType(TargetType.RDS_INSTANCE).withRdsResourceId("instanceId")))
+                .when(proxy).injectCredentialsAndInvoke(eq(targetRequest), any());
+
+        final ResourceModel model = ResourceModel.builder().dBProxyName(PROXY_NAME).targetGroupName(DEFAULT_NAME).build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                                                                      .desiredResourceState(model)
+                                                                      .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThatModelsAreEqual(response.getResourceModel(), dbProxyTargetGroup1);
+        assertThat(response.getResourceModel().getDBClusterIdentifiers()).isNotNull();
+        assertThat(response.getResourceModel().getDBClusterIdentifiers()).isEqualTo(ImmutableList.of(clusterId));
+        assertThat(response.getResourceModel().getDBInstanceIdentifiers()).isNull();
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
     }
